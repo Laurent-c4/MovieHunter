fac91763267bed3285280b058990e506ca13abec
Laurent Juma <43743544+Laurent-c4@users.noreply.github.com>
2023-09-28 21:01:03 +0100

    Fix default tests

--- app/src/androidTest/java/com/c4entertainment/moviehunter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/c4entertainment/moviehunter/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.frogtest.movieguru", appContext.packageName)
+        assertEquals("com.c4entertainment.moviehunter", appContext.packageName)
     }
 }
